bd6514554ea5a5ef203fbbf64e39a5efb37346a9
KyO <yong931231@hotmail.com>
2014-04-03 07:21:50 +0800

    Add test to xorlw to verify operand1.

--- src/XORLW.c
+++ src/XORLW.c
@@ -5,5 +5,10 @@
 char FSR[0x1000];
 
 void xorlw(Bytecode *code){
+
+	if((code->operand1<=255)&&(code->operand1>=0))
+		FSR[WREG] = FSR[WREG] ^ code->operand1;
+	else
+		Throw(ERR_INVALID_OPERAND1);
 	
 }
--- test/test_XORLW.c
+++ test/test_XORLW.c
@@ -6,6 +6,72 @@
 void setUp(){}
 void tearDown(){}
 
-void test_xorlw(){
+void test_xorlw_with_invalid_operand1_negative1_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with -1
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = -1,
+					 .operand2 = -1,
+					 .operand3 = -1
+					 };
+	
+	FSR[WREG] = 0xB5;
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
+
+void test_xorlw_with_invalid_operand1_256_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 256
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 256,
+					 .operand2 = -1,
+					 .operand3 = -1
+					 };
+	
+	FSR[WREG] = 0xB5;
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
+
+void test_xorlw_with_valid_operand1_0x0A_should_xor_to_WREG_return_0xBF(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 0x0A
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 0x0A,
+					 .operand2 = -1,
+					 .operand3 = -1
+					 };
+	
+	FSR[WREG] = 0xB5;
+	//00001010 -> 0A
+	//10110101 -> B5
+	//10111111 -> BF
+	
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xBF,FSR[WREG]);
 
 }
