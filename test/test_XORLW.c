bd2589956899c1789dc2a620ca7ac1d3c6f0928b
KyO <yong931231@hotmail.com>
2014-04-03 07:28:02 +0800

    Add test to xorlw to verify validity of operand 2 and 3. Should throw exception if there are not empty.

--- test/test_XORLW.c
+++ test/test_XORLW.c
@@ -75,3 +75,136 @@ void test_xorlw_with_valid_operand1_0x0A_should_xor_to_WREG_return_0xBF(){
 	TEST_ASSERT_EQUAL_HEX8(0xBF,FSR[WREG]);
 
 }
+
+void test_xorlw_with_non_empty_operand2_0x0A_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 0x0A
+	//-1 represent empty
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 0x0A,
+					 .operand2 = 0,
+					 .operand3 = -1
+					 };
+	
+	FSR[WREG] = 0xB5;
+	//00001010 -> 0A
+	//10110101 -> B5
+	//10111111 -> BF
+	
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND2,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
+
+void test_xorlw_with_non_empty_operand2_F_0x0A_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 0x0A
+	//-1 represent empty
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 0x0A,
+					 .operand2 = F,
+					 .operand3 = -1
+					 };
+	
+	FSR[WREG] = 0xB5;
+	//00001010 -> 0A
+	//10110101 -> B5
+	//10111111 -> BF
+	
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND2,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
+
+void test_xorlw_with_non_empty_operand2_W_0x0A_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 0x0A
+	//-1 represent empty
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 0x0A,
+					 .operand2 = W,
+					 .operand3 = -1
+					 };
+	
+	FSR[WREG] = 0xB5;
+	//00001010 -> 0A
+	//10110101 -> B5
+	//10111111 -> BF
+	
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND2,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
+
+void test_xorlw_with_non_empty_operand3_BANKED_0x0A_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 0x0A
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 0x0A,
+					 .operand2 = -1,
+					 .operand3 = BANKED
+					 };
+	
+	FSR[WREG] = 0xB5;
+	//00001010 -> 0A
+	//10110101 -> B5
+	//10111111 -> BF
+	
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND3,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
+
+void test_xorlw_with_non_empty_operand3_ACCESS_0x0A_should_throw_exception(){
+	CEXCEPTION_T operandERR;
+	
+	//Test fixture
+	//Initialize WREG with B5h and XOR with 0x0A
+	Bytecode code = {.instruction = {.mnemonic = XORLW, .name = "xorlw"},
+					 .operand1 = 0x0A,
+					 .operand2 = -1,
+					 .operand3 = ACCESS
+					 };
+	
+	FSR[WREG] = 0xB5;
+	//00001010 -> 0A
+	//10110101 -> B5
+	//10111111 -> BF
+	
+	Try{
+		xorlw(&code);
+	} Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND3,operandERR);
+	}
+	
+	TEST_ASSERT_EQUAL_HEX8(0xB5,FSR[WREG]);
+
+}
